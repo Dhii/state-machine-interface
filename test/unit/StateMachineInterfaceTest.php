8d4929c17b56700f0772d12931360dc5f8291846
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-25 18:39:50 +0200

    Added test for `StateMachineInterface`

---
+++ test/unit/StateMachineInterfaceTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Dhii\Machine\UnitTest;
+
+use Dhii\Machine\StateMachineInterface as TestSubject;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class StateMachineInterfaceTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Machine\StateMachineInterface';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return TestSubject
+     */
+    public function createInstance()
+    {
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+                     ->getStatus()
+                     ->transition()
+                     ->canTransition();
+
+        return $mock->new();
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'Test subject does not implement parent interface.'
+        );
+
+        $this->assertInstanceOf(
+            static::TEST_SUBJECT_CLASSNAME,
+            $subject,
+            'Test subject does not implement parent interface.'
+        );
+    }
+}
