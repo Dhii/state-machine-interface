e2ce28bdbb228256b37c15899f3996d8996fc134
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-25 18:40:00 +0200

    Added `StateMachineInterface`

---
+++ src/StateMachineInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Dhii\Machine;
+
+use Dhii\Machine\Exception\CouldNotTransitionExceptionInterface;
+use Dhii\Machine\Exception\StateMachineExceptionInterface;
+use Dhii\Util\String\StringableInterface as Stringable;
+
+/**
+ * Something that represents a state machine.
+ *
+ * @since [*next-version*]
+ */
+interface StateMachineInterface extends StatusAwareInterface
+{
+    /**
+     * Applies a transition.
+     *
+     * Implementations are NOT bound to returning the same state machine instance.
+     * Depending on the same instance is not recommended.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable $transition The transition code.
+     *
+     * @throws CouldNotTransitionExceptionInterface If the transition failed or was aborted.
+     * @throws StateMachineExceptionInterface       If an error was encountered during transition.
+     *
+     * @return StateMachineInterface The state machine with the new state.
+     */
+    public function transition($transition);
+
+    /**
+     * Checks if a transition can be applied.
+     *
+     * This method does not guarantee that the transition will be successful.
+     * It only indicates that a given transition can, at the very least, be attempted.
+     * The transition, when applied, is still permitted to fail and throw an exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable $transition The transition to check.
+     *
+     * @return bool True if the transition is possible, false if not.
+     */
+    public function canTransition($transition);
+}
