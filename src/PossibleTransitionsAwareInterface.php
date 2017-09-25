3f9f591a2e078a9cfed50933689136387f6de5fc
Miguel Muscat <miguelmuscat93@gmail.com>
2017-09-25 18:54:42 +0200

    Added `PossibleTransitionsAwareInterface`

---
+++ src/PossibleTransitionsAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Dhii\Machine;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use Traversable;
+
+/**
+ * Something that is aware of, and can provide, a list of transitions possible given its current state.
+ *
+ * @since [*next-version*]
+ */
+interface PossibleTransitionsAwareInterface
+{
+    /**
+     * Gets the currently possible transitions.
+     *
+     * @since [*next-version*]
+     *
+     * @return string[]|Stringable[]|Traversable The currently possible transitions.
+     */
+    public function getPossibleTransitions();
+}
