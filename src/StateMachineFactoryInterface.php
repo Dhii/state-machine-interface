ed1adcdbca8cec0618e3027bac28e3a1cfeb5fd4
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-10 01:55:05 +0200

    Upgrade `StateMachineFactoryInterface`
    - Change the generic `make()` in favour of a more specific signature.
    - Uses templates, meaning the single factory interface can represent a factory of any `StateMachineInterface` subtype, while still preserving relative type-safety with Psalm.
    - Allowed to throw.

--- src/StateMachineFactoryInterface.php
+++ src/StateMachineFactoryInterface.php
@@ -2,19 +2,36 @@
 
 namespace Dhii\State;
 
+use Exception;
+use RuntimeException;
+use Stringable;
+
 /**
  * Something that can create new state machine instances.
  *
  * @since [*next-version*]
+ *
+ * @psalm-import-type State of StateMachineInterface
+ * @psalm-import-type Transition of StateMachineInterface
+ * @template SM of StateMachineInterface
  */
 interface StateMachineFactoryInterface
 {
     /**
-     * {@inheritdoc}
+     * Creates a new state machine.
      *
      * @since [*next-version*]
      *
+     * @param array<string|Stringable, iterable<string|Stringable>> $map
+     * @psam-param array<string|Stringable, iterable<Transition>> $map
+     * @param string|Stringable $initialState
+     * @psam-param  State $initialState
+     *
      * @return StateMachineInterface The created state machine.
+     * @psalm-return SM
+     *
+     * @throws RuntimeException If could not create.
+     * @throws Exception If problem creating.
      */
-    public function make($config = null);
+    public function createStateMachineFromStateMap(array $map, $initialState): StateMachineInterface;
 }
